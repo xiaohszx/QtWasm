c10cc4d437193bb9d29cfad982ced45309dd5883
Fredrik Orderud <forderud@gmail.com>
2020-04-08 10:34:57 +0200

    Switch to debug builds.

--- run_qmake.bat
+++ run_qmake.bat
@@ -2,6 +2,6 @@ mkdir C:\Dev\QtWasm\build
 
 :: Input sources must be found in C:\Dev\QtWasm\source
 :: Output binaries will be written to C:\Dev\QtWasm\build
-docker run --rm -v C:\Dev\QtWasm\source:/project/source -v C:\Dev\QtWasm\build:/project/build forderud/qtwasm:latest /bin/bash -c "export PATH=$PATH:/project/Qt/bin;qmake /project/source && make"
+docker run --rm -v C:\Dev\QtWasm\source:/project/source -v C:\Dev\QtWasm\build:/project/build forderud/qtwasm:latest /bin/bash -c "export PATH=$PATH:/project/Qt/bin;qmake /project/source CONFIG+=debug && make"
 
 pause
